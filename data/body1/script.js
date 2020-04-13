e947b393e512f389a0b968e554b003864d1dba54
minzhang190 <minzhang190@gmail.com>
2020-04-13 11:03:15 +0000

    +body1/2

---
+++ data/body1/script.js
@@ -0,0 +1,19 @@
+document.title = "Shēntǐ";
+
+loadSound([
+    "5e943b2b25c505681d0f1dee.mp3",
+    "5e8dac932c02d5b61d430631.mp3",
+    "5e8dac932c02d5b61d43063c.mp3",
+    "5e8dac932c02d5b61d43062e.mp3",
+    "5e8dac932c02d5b61d43062f.mp3",
+    "5e943b2d25c505681d0f1def.mp3",
+    "5e8dac932c02d5b61d43062d.mp3",
+    "5e8dac932c02d5b61d430630.mp3",
+    "5e943b2e25c505681d0f1df0.mp3",
+    "5e8dac932c02d5b61d43063d.mp3",
+    "5e8dac932c02d5b61d43063b.mp3",
+    "5e8dac932c02d5b61d430638.mp3",
+    "5e8dac932c02d5b61d43062c.mp3",
+    "5e8dac932c02d5b61d43063f.mp3",
+    "5e8dac932c02d5b61d430639.mp3"
+]);
---
+++ data/body2/script.js
@@ -0,0 +1,19 @@
+document.title = "Shēntǐ";
+
+loadSound([
+    "5e8dac932c02d5b61d43063a.mp3",
+    "5e943b3125c505681d0f1df1.mp3",
+    "5e8dac932c02d5b61d430636.mp3",
+    "5e943b3225c505681d0f1df2.mp3",
+    "5e943b3225c505681d0f1df3.mp3",
+    "5e8dac932c02d5b61d430634.mp3",
+    "5e8dac932c02d5b61d430633.mp3",
+    "5e8dac932c02d5b61d430635.mp3",
+    "5e8dac932c02d5b61d430632.mp3",
+    "5e943b3425c505681d0f1df4.mp3",
+    "5e943b3425c505681d0f1df5.mp3",
+    "5e8dac932c02d5b61d430637.mp3",
+    "5e8dac932c02d5b61d43063e.mp3",
+    "5e943b3625c505681d0f1df6.mp3",
+    "5e943b3625c505681d0f1df7.mp3"
+]);
